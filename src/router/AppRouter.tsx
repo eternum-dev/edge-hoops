a5c51243497828337d8240b3f2bd25cc1a89324d
eternum-dev <alejandro.thon.j@gmail.com>
2025-06-25 19:15:20 -0400

    feat(router): add multiple routes to AppRouter with real pages

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,9 +1,20 @@
-import { Route, Routes } from "react-router"
+import { Route, Routes } from "react-router";
+import {
+  HomeDashboardPage,
+  Error404Page,
+  FavoritesPage,
+  GameDetailPage,
+  TeamPage,
+} from "../component/pages";
 
 export const AppRouter = () => {
   return (
     <Routes>
-        <Route path={"/"} element={<h1>Hola Mundo desdes react-router</h1>} />
+      <Route path={"/"} element={<HomeDashboardPage />} />
+      <Route path={"/game"} element={<GameDetailPage />} />
+      <Route path={"/team"} element={<TeamPage />} />
+      <Route path={"/404"} element={<Error404Page />} />
+      <Route path={"/favorites"} element={<FavoritesPage />} />
     </Routes>
-  )
-}
+  );
+};
