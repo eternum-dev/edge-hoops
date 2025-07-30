6a0e674dde06f9bc156a144bace6b25a90d1e958
eternum-dev <alejandro.thon.j@gmail.com>
2025-07-29 21:20:54 -0400

    feat(atom): add atomic DashedLine component

---
+++ src/component/atoms/DashedLine.tsx
@@ -0,0 +1,7 @@
+export const DashedLine: React.FC = () => {
+  return (
+    <div className="h-[5rem] w-[3rem] p-2">
+      <div className="border-l-1 border-dashed border-neutral-1000 h-full w-[1px]" />
+    </div>
+  );
+};
