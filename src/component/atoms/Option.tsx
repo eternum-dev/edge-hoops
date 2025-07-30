bbfa9b9003ecfc4f95fa54a1113f02b60cdb0f44
eternum-dev <alejandro.thon.j@gmail.com>
2025-07-29 21:31:09 -0400

    feat(atom): add atomic Option component with textPosition, disable and selected variants

---
+++ src/component/atoms/Option.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+type OptionTextPosition = "left" | "right";
+
+interface OptionProps {
+  textPosition?: OptionTextPosition;
+  className?: string;
+  icon?: string;
+  children?: React.ReactNode;
+  disable?: boolean;
+}
+
+const defaultStyles: string =
+  "font-inter font-medium text-b2 leading-b2 px-3 py-2 bg-neutral-100 border-1 border-neutral-300 flex justify-between w-full text-neutral-1000 rounded-2xs hover:text-neutral-100 hover:bg-secondary-200 active:text-neutral-100 active:bg-secondary-300  active:bg-secondary-300";
+const selectedStyles: string =
+  "text-neutral-1000 bg-neutral-100 border-1 border-l-4 border-secondary-300";
+const disableStyles: string = "!text-neutral-900 !bg-neutral-200 !border-1 !border-neutral-300";
+const baseStyles: Record<OptionTextPosition, string> = {
+  left: "flex-row",
+  right: "flex-row-reverse",
+};
+
+export const Option: React.FC<OptionProps> = ({
+  textPosition = "left",
+  className = "",
+  icon = "img",
+  children = "Option",
+  disable = false,
+}) => {
+  const [selected, setSelected] = useState<boolean>(false);
+  return (
+    <div
+      className={`${defaultStyles} ${baseStyles[textPosition]} ${className} 
+       ${selected && selectedStyles} ${disable && disableStyles}`}
+      onClick={() => setSelected((prev) => !prev)}
+    >
+      <span className="font-inter font-medium text-b2 leading-b2">
+        {children}
+      </span>
+      {icon && icon}
+    </div>
+  );
+};
