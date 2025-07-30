c3df578465647e1b2479a5f9fa6f7701a9c0bf02
eternum-dev <alejandro.thon.j@gmail.com>
2025-07-29 21:23:29 -0400

    feat(atom): add atomic Divider component with size  variants

---
+++ src/component/atoms/Divider.tsx
@@ -0,0 +1,21 @@
+type DividerSize = "s" | "m" | "l";
+
+interface DividerProps {
+  className?: string;
+  size?: DividerSize;
+}
+
+const sizeStyled: Record<DividerSize, string> = {
+  l: "h-[3px]",
+  m: "h-[2px]",
+  s: "h-[1px]",
+};
+
+export const Divider: React.FC<DividerProps> = ({
+  className = "",
+  size = "s",
+}) => {
+  return (
+    <div className={` ${sizeStyled[size]} ${className} w-full  bg-neutral-1000 my-auto mx-1`}></div>
+  );
+};
