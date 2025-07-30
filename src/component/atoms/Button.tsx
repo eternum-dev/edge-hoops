99c2a6248c885fd3125bb4d62981b213b6c587fa
eternum-dev <alejandro.thon.j@gmail.com>
2025-07-29 21:20:07 -0400

    feat(atom): add atomic Button component with size and color variants

---
+++ src/component/atoms/Button.tsx
@@ -0,0 +1,39 @@
+type ButtonSize = "fit" | "max";
+type ButtonColor = "primary" | "secondary";
+
+interface ButtonProps {
+  size?: ButtonSize;
+  color?: ButtonColor;
+  className?: string;
+  children: React.ReactNode;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const baseStyles: Record<ButtonSize, string> = {
+  fit: "rounded-2xs font-inter font-regular leading-b3 text-b3 pt-(--spacing-2) pb-(--spacing-2) pr-(--spacing-4) pl-(--spacing-4) h-min",
+  max: "rounded-2xs font-inter font-regular leading-b3 text-b3 pt-(--spacing-2) pb-(--spacing-2) pr-(--spacing-4) pl-(--spacing-4) h-full max-h-[45px]",
+};
+
+const colorStyles: Record<ButtonColor, string> = {
+  primary:
+    "bg-primary-300 text-neutral-100 hover:bg-primary-400 active:bg-primary-200 disabled:bg-neutral-300 disabled:text-neutral-900",
+  secondary:
+    "bg-secondary-300 text-neutral-100 hover:bg-secondary-400 active:bg-primary-200 disabled:bg-neutral-300 disabled:text-neutral-900",
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  size = "fit",
+  color = "primary",
+  className = "",
+  children = "00/00/00",
+  onClick,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${baseStyles[size]} ${colorStyles[color]} ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
