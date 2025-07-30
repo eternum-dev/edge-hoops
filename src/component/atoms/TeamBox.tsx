fd7d60eb38593839fddd9dadc9873be46bc5c572
eternum-dev <alejandro.thon.j@gmail.com>
2025-07-29 21:38:51 -0400

    feat(atom): add atomic TeamBox component with color variants

---
+++ src/component/atoms/TeamBox.tsx
@@ -0,0 +1,21 @@
+type TeamBoxColor = "primary" | "secondary";
+
+interface TeamBoxProps {
+  color?: TeamBoxColor;
+  className?: string;
+}
+
+const colorStyles: Record<TeamBoxColor, string> = {
+  primary: "bg-primary-300 font-oswald font-bold text-h7 leading-h7",
+  secondary: "bg-secondary-300 font-inter font-medium text-b1 leading-b1",
+};
+
+const baseStyle = "text-neutral-900 w-[22px] h-[22px] rounded-2xs"
+
+export const TeamBox: React.FC<TeamBoxProps> = ({ color="primary", className="" }) => {
+  return (
+    <div
+      className={`${baseStyle} ${colorStyles[color]} ${className} `}
+    ></div>
+  );
+};
