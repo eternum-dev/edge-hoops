86cb94d1d1f6cb9b4e2092128d3853b775bf458f
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-04 23:14:41 -0400

    feat(atoms): update Date component with textColor support and simplified size values

--- src/component/atoms/Date.tsx
+++ src/component/atoms/Date.tsx
@@ -1,20 +1,34 @@
-type DateSize = "max" | "fit";
+export type DateSize = "s" | "m";
+export type DateTextColor = "white" | "black";
 
 interface DateProps {
   size?: DateSize;
   className?: string;
   children: React.ReactNode;
+  textColor?: DateTextColor;
 }
 
 const baseStyles: Record<DateSize, string> = {
-  max: "font-oswald font-bold leading-h7 text-h7",
-  fit: "font-inter font-medium leading-b1 text-b1",
+  m: "font-oswald font-bold leading-h7 text-h7",
+  s: "font-inter font-medium leading-b1 text-b1",
+};
+
+const TextColorStyles: Record<DateTextColor, string> = {
+  black: "text-neutral-1000",
+  white: "text-neutral-100",
 };
 
 export const Date: React.FC<DateProps> = ({
-  size = "fit",
+  size = "s",
   className = "",
   children,
+  textColor = "black",
 }) => {
-  return <p className={`${baseStyles[size]}${className}`}>{children}</p>;
+  return (
+    <p
+      className={`${baseStyles[size]} ${TextColorStyles[textColor]} ${className}`}
+    >
+      {children}
+    </p>
+  );
 };
