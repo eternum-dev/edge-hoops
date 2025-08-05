4e655857ef5f7773d80feae103305f34e02c496f
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-04 23:21:20 -0400

    feat(molecules): add TeamColorBox component to show team logos with associated colors

---
+++ src/component/molecules/TeamColorBox.tsx
@@ -0,0 +1,26 @@
+import type { ShortTeamNames } from "../../types/nba";
+import { Divider, ScuareColor, TeamLogo } from "../atoms";
+
+interface TeamColorBoxProps {
+  leftTeam: ShortTeamNames;
+  rightTeam: ShortTeamNames;
+}
+
+export const TeamColorBox: React.FC<TeamColorBoxProps> = ({
+  leftTeam = "BOS",
+  rightTeam = "BRK",
+}) => {
+  return (
+    <div className="flex px-2 py-1 gap-2">
+      <div className="flex gap-1">
+        <ScuareColor colorBox="orange" />
+        <TeamLogo size="verySmall" team={leftTeam} />
+      </div>
+      <Divider className="w-[16px]!" />
+      <div className="flex gap-1">
+        <TeamLogo size="verySmall" team={rightTeam} />
+        <ScuareColor colorBox="blue" />
+      </div>
+    </div>
+  );
+};
