04d058c36498bd6f32fb36c3bedec893e91a27f9
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-08 19:00:40 -0400

    refactor(types): add PositionLogo,  MoleculesSize and and regroups the types

--- src/types/ui.ts
+++ src/types/ui.ts
@@ -1,24 +1,33 @@
-export type WidthSize = "fit" | "max";
-
-export type DateSize = "s" | "m";
-
+// text
 export type TextColor = "white" | "black";
 export type TextSize = "s" | "m" | "l" | "xl";
-
-export type DividerHeigth = "s" | "m" | "l";
-
-export type TypeColor = "primary" | "secondary";
-
-export type IconNames = "Arrow" | "Fire";
-
 export type TextPosition = "left" | "right";
 
+// dates
+export type DateSize = "s" | "m";
+
+// divider
+export type DividerHeigth = "s" | "m" | "l";
+
+// colors
+export type TypeColor = "primary" | "secondary";
 export type NeutralColor = "white" | "black";
 
+// table
 export type CellType = "header" | "body";
 export type CellContentType = "text" | "date" | "team";
 
+// logos
 export type TeamLogoSize = "verySmall" | "small" | "medium" | "full";
-export type IconSize = "small" | "medium" | "full";
+export type PositionLogo = "left" | "right";
 
+// icons
+export type IconSize = "small" | "medium" | "full";
+export type IconNames = "Arrow" | "Fire";
+
+// tags
 export type TitleTagType = "h1" | "h2" | "h3" | "h4";
+
+// component
+export type MoleculesSize = "small" | "full";
+export type WidthSize = "fit" | "max";
