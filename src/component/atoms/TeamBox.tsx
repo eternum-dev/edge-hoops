c9fddd6e9e2e6bad552d617bca32f367b23df16b
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-09 18:50:07 -0400

    refactor(team-box): use shared types for color

--- src/component/atoms/TeamBox.tsx
+++ src/component/atoms/TeamBox.tsx
@@ -1,21 +1,22 @@
-type TeamBoxColor = "primary" | "secondary";
+import type { TypeColor } from "../../types";
 
 interface TeamBoxProps {
-  color?: TeamBoxColor;
+  color?: TypeColor;
   className?: string;
 }
 
-const colorStyles: Record<TeamBoxColor, string> = {
+const colorStyles: Record<TypeColor, string> = {
   primary: "bg-primary-300 font-oswald font-bold text-h7 leading-h7",
   secondary: "bg-secondary-300 font-inter font-medium text-b1 leading-b1",
 };
 
-const baseStyle = "text-neutral-900 w-[22px] h-[22px] rounded-2xs"
+const baseStyle = "text-neutral-900 w-[22px] h-[22px] rounded-2xs";
 
-export const TeamBox: React.FC<TeamBoxProps> = ({ color="primary", className="" }) => {
+export const TeamBox: React.FC<TeamBoxProps> = ({
+  color = "primary",
+  className = "",
+}) => {
   return (
-    <div
-      className={`${baseStyle} ${colorStyles[color]} ${className} `}
-    ></div>
+    <div className={`${baseStyle} ${colorStyles[color]} ${className} `}></div>
   );
 };
