28c0a6fa0d207352a2c850380c29058a81145b77
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-12 23:39:47 -0400

    refactor(dashed-line): remove unnecessary classname and update border color

--- src/component/atoms/DashedLine.tsx
+++ src/component/atoms/DashedLine.tsx
@@ -1,7 +1,7 @@
 export const DashedLine: React.FC = () => {
   return (
-    <div className="h-[5rem] w-[3rem] p-2">
-      <div className="border-l-1 border-dashed border-neutral-1000 h-full w-[1px]" />
+    <div className="p-2">
+      <div className="border-l-1 border-dashed  border-neutral-800 h-full w-[1px]" />
     </div>
   );
 };
