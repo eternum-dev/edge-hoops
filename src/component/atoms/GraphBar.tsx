a3ba9ef707d5dbc297cc7e6e3fd92c01dce3da7a
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-12 23:41:49 -0400

    refactor(graph-bar): update props, change baseStyles and fix height

--- src/component/atoms/GraphBar.tsx
+++ src/component/atoms/GraphBar.tsx
@@ -4,15 +4,15 @@ import { Text } from "./Text";
 interface GraphBarProps {
   className?: string;
   color?: TypeColor;
-  height?: string;
+  height: string | number;
   value: string | number;
 }
 
 const BaseStyled: Record<TypeColor, string> = {
   primary:
-    "w-full max-w-[35px] h-full rounded-2xs bg-primary-300 drop-shadow-neutral-1000 drop-shadow-(--shadow-1) ",
+    "w-full min-w-[35px] h-full rounded-2xs bg-primary-300 drop-shadow-neutral-1000 drop-shadow-(--shadow-1) ",
   secondary:
-    "w-full max-w-[35px] h-full rounded-2xs bg-secondary-300 drop-shadow-neutral-900 drop-shadow-sm",
+    "w-full min-w-[35px] h-full rounded-2xs bg-secondary-300 drop-shadow-neutral-900 drop-shadow-sm",
 };
 
 export const GraphBar: React.FC<GraphBarProps> = ({
@@ -21,11 +21,13 @@ export const GraphBar: React.FC<GraphBarProps> = ({
   height,
   value = "1000",
 }) => {
+  const newHeight = Number(height) / 100;
+
   return (
-    <div className="flex flex-col w-full h-full items-center">
+    <div className="flex flex-col w-full h-full justify-end">
       <div
-        style={{ height: height }}
-        className={`${className}${BaseStyled[color]}`}
+        style={{ height: `calc(calc(100% - 30px) * ${newHeight} )` }}
+        className={`${className} ${BaseStyled[color]} `}
       ></div>
       <Text>{value}</Text>
     </div>
