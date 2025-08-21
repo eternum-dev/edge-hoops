8175f8a2c88a11f5cb57020fcd6a2526c9003661
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-21 19:11:05 -0400

    refactor(graph-bar): change styles and add size text component - replace the minimum width for the bar `min-w-[15px]`, - add `items-center` classname of container, - add  `s` size of  text component.

--- src/component/atoms/GraphBar.tsx
+++ src/component/atoms/GraphBar.tsx
@@ -10,9 +10,9 @@ interface GraphBarProps {
 
 const BaseStyled: Record<TypeColor, string> = {
   primary:
-    "w-full min-w-[35px] h-full rounded-2xs bg-primary-300 drop-shadow-neutral-1000 drop-shadow-(--shadow-1) ",
+    "w-full min-w-[15px] h-full rounded-2xs bg-primary-300 drop-shadow-neutral-1000 drop-shadow-(--shadow-1) ",
   secondary:
-    "w-full min-w-[35px] h-full rounded-2xs bg-secondary-300 drop-shadow-neutral-900 drop-shadow-sm",
+    "w-full min-w-[15px] h-full rounded-2xs bg-secondary-300 drop-shadow-neutral-900 drop-shadow-sm",
 };
 
 export const GraphBar: React.FC<GraphBarProps> = ({
@@ -24,12 +24,12 @@ export const GraphBar: React.FC<GraphBarProps> = ({
   const newHeight = Number(height) / 100;
 
   return (
-    <div className="flex flex-col w-full h-full justify-end">
+    <div className="flex flex-col w-full h-full justify-end items-center">
       <div
         style={{ height: `calc(calc(100% - 30px) * ${newHeight} )` }}
         className={`${className} ${BaseStyled[color]} `}
       ></div>
-      <Text>{value}</Text>
+      <Text size="s">{value}</Text>
     </div>
   );
 };
