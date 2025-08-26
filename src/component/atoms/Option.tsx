e591d618e8bf2a4cd7070f0bca6f9ef2c16d936a
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-25 22:58:07 -0400

    refactor(option): add `items-center` className in to container component

--- src/component/atoms/Option.tsx
+++ src/component/atoms/Option.tsx
@@ -12,7 +12,7 @@ interface OptionProps {
 }
 
 const defaultStyles: string =
-  "font-inter font-medium text-b2 leading-b2 px-3 py-2 bg-neutral-100 border-1 border-neutral-300 flex justify-between w-full text-neutral-1000 rounded-2xs hover:text-neutral-100 hover:bg-secondary-200 active:text-neutral-100 active:bg-secondary-300  active:bg-secondary-300";
+  "font-inter font-medium text-b2 leading-b2 px-2 py-1 bg-neutral-100 border-1 border-neutral-300 flex justify-between w-full text-neutral-1000 rounded-2xs hover:text-neutral-100 hover:bg-secondary-200 active:text-neutral-100 active:bg-secondary-300  active:bg-secondary-300 items-center";
 const selectedStyles: string =
   "text-neutral-1000 bg-neutral-100 border-1 border-l-4 border-secondary-300";
 const disableStyles: string =
@@ -25,7 +25,7 @@ const baseStyles: Record<TextPosition, string> = {
 export const Option: React.FC<OptionProps> = ({
   textPosition = "left",
   className = "",
-  icon = "BOS",
+  icon,
   children = "Option",
   disable = false,
 }) => {
