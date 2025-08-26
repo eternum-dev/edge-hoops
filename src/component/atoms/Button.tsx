337e95fdd88d97c1302bba8fbb29387a57f15989
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-25 23:03:28 -0400

    refactor(button): add `type` props and fix styles

--- src/component/atoms/Button.tsx
+++ src/component/atoms/Button.tsx
@@ -1,4 +1,4 @@
-import type { TypeColor, WidthSize } from "../../types";
+import type { ButtonType, TypeColor, WidthSize } from "../../types";
 
 interface ButtonProps {
   size?: WidthSize;
@@ -6,11 +6,12 @@ interface ButtonProps {
   className?: string;
   children: React.ReactNode;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
+  type: ButtonType;
 }
 
 const baseStyles: Record<WidthSize, string> = {
-  fit: "rounded-2xs font-inter font-regular leading-b3 text-b3 py-(--spacing-1) px-(--spacing-2) h-full",
-  max: "rounded-2xs font-inter font-regular leading-b3 text-b3 py-(--spacing-2) px-(--spacing-4) h-full max-h-[45px]",
+  fit: "rounded-2xs font-inter font-regular leading-b3 text-b3 px-2 py-1.5 h-fit",
+  max: "rounded-2xs font-inter font-regular leading-b3 text-b3 px-2 py-1.5 h-full max-h-[45px]",
 };
 
 const colorStyles: Record<TypeColor, string> = {
@@ -26,11 +27,13 @@ export const Button: React.FC<ButtonProps> = ({
   className = "",
   children = "00/00/00",
   onClick,
+  type = "button",
 }) => {
   return (
     <button
       onClick={onClick}
       className={`${baseStyles[size]} ${colorStyles[color]} ${className}`}
+      type={type}
     >
       {children}
     </button>
