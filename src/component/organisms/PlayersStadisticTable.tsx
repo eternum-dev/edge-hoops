19d96955d8ec674c738b171ab1c9b40d207815be
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-26 22:28:10 -0400

    feat(molecules): add `PlayersStadisticTable` component with title, tableData and glossary props

---
+++ src/component/organisms/PlayersStadisticTable.tsx
@@ -0,0 +1,35 @@
+import { Title } from "../atoms";
+import { ColumTable } from "../molecules";
+import type { DefinitionProps, TableData } from "../../types";
+import { StatsGlossary } from "./StatsGlossary";
+
+interface PlayersStadisticTableProps {
+  title?: string;
+  tableData: TableData[];
+  glossary: DefinitionProps[];
+}
+
+export const PlayersStadisticTable: React.FC<PlayersStadisticTableProps> = ({
+  title = "",
+  tableData = [],
+  glossary = [],
+}) => {
+  return (
+    <section className="flex flex-col w-full gap-4 bg-neutral-100 p-5 rounded-3xl shadow-md shadow-neutral-700">
+      <header className="flex justify-between items-center">
+        <Title type="h2">{title ? title : "Estadisticas de los jugadores"}</Title>
+      </header>
+      <div className="flex">
+        {tableData.map((item, index) => (
+          <ColumTable
+            cellContent={item.body}
+            headerText={item.header}
+            typeColum={item.type}
+            key={index}
+          />
+        ))}
+      </div>
+      <StatsGlossary glossaryData={glossary} />
+    </section>
+  );
+};
