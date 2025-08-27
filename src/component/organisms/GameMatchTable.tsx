2c46854a1c35c24bee065f8a37276c3705bc88ec
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-26 22:20:26 -0400

    feat(molecules): add `GameMatchTable` component with title, tableData, gamesButtons and changeGraph props

---
+++ src/component/organisms/GameMatchTable.tsx
@@ -0,0 +1,50 @@
+import { Button, Title } from "../atoms";
+import { ColumTable } from "../molecules";
+import type { TableData } from "../../types";
+
+interface GameMatchTableProps {
+  title?: string;
+  tableData: TableData[];
+  gamesButtons: string[];
+  changeGraph: (idButton: string) => void;
+}
+
+export const GameMatchTable: React.FC<GameMatchTableProps> = ({
+  title = "",
+  tableData,
+  gamesButtons,
+  changeGraph,
+}) => {
+  return (
+    <section className="flex flex-col w-full gap-4 bg-neutral-100 p-5 rounded-3xl shadow-md shadow-neutral-700">
+      <header className="flex justify-between items-center">
+        <Title type="h2">{title ? title : "Lista de partidos"} </Title>
+      </header>
+      <div className="flex gap-1">
+        <div className="flex w-full">
+          {tableData.map((item, index) => (
+            <ColumTable
+              cellContent={item.body}
+              headerText={item.header}
+              typeColum={item.type}
+              key={index}
+            />
+          ))}
+        </div>
+        <div className="grid row-auto w-[36px] min-h-full gap-[1px]">
+          <div className="w-full h-[30px]"></div>
+          {gamesButtons.map((button) => (
+            <Button
+              size="fit"
+              onClick={() => changeGraph(button)}
+              className="h-full"
+              type="button"
+            >
+              ir
+            </Button>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
