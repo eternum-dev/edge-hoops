0ef2eff22f27b007921749350cfbcaa5e1cc98b2
eternum-dev <alejandro.thon.j@gmail.com>
2025-08-30 18:57:22 -0400

    refactor(title): add styles to for the new sizes and change size in to h4 styles

--- src/component/atoms/Title.tsx
+++ src/component/atoms/Title.tsx
@@ -11,7 +11,8 @@ const baseStyles: Record<TitleTagType, string> = {
   h1: "text-h1 leading-h1 font-bold font-oswald",
   h2: "text-h4 leading-h4 font-bold font-oswald",
   h3: "text-h5 leading-h5 font-bold font-oswald",
-  h4: "text-h7 leading-h7 font-medium font-oswald",
+  h4: "text-h6 leading-h6 font-bold font-oswald",
+  h5: "text-b2 leading-b2 font-medium font-oswald",
 };
 
 const colorStyles: Record<TextColor, string> = {
