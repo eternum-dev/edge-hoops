f1aacb4b7d0622cb787b8a5b0ae8ba7433388a46
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-01 22:37:32 -0400

    refactor(mvp-card): add `className` prop abd change styles

--- src/component/organisms/MvpCard.tsx
+++ src/component/organisms/MvpCard.tsx
@@ -1,13 +1,18 @@
 import type { PlayerData } from "../../types";
-import { Image, Text, Title } from "../atoms";
+import { Image, Span, Text, Title } from "../atoms";
 import { StatBox } from "../molecules";
 
-interface MvpCardProps {
+export interface MvpCardProps {
   title?: string;
   playerData: PlayerData;
+  className?: string;
 }
 
-export const MvpCard: React.FC<MvpCardProps> = ({ title, playerData }) => {
+export const MvpCard: React.FC<MvpCardProps> = ({
+  title,
+  playerData,
+  className = "",
+}) => {
   const {
     imagePlayer,
     leftStats,
@@ -18,25 +23,32 @@ export const MvpCard: React.FC<MvpCardProps> = ({ title, playerData }) => {
   } = playerData;
 
   return (
-    <section className="flex flex-col gap-4 p-5 rounded-3xl bg-neutral-100">
+    <section
+      className={`w-full flex flex-col gap-4 p-5 rounded-3xl bg-neutral-100 shadow-neutral-700 shadow-md ${className}`}
+    >
       <header>
         <Title type="h2">{title ? title : "Este es el titulo"}</Title>
       </header>
-      <div className="flex gap-1  w-full justify-between items-center">
-        <div className="flex flex-col gap-2">
+      <div className="flex gap-1 h-full w-full justify-between items-center">
+        <div className="flex flex-col gap-2 mt-auto">
           {leftStats.map(({ label, value }) => (
-            <StatBox title={label} value={value} textPosition="left" />
+            <StatBox title={label} value={value} color="blue" textPosition="left" />
           ))}
         </div>
-        <div className="w-full flex flex-col gap-1 px-2 py-0 items-center justify-between">
-          <Title type="h3">{namePlayer}</Title>
-          <Image src={imagePlayer.src} alt={imagePlayer.alt} />
-          <Text size="m">{positionPlayer}</Text>
-          <Text size="s">{shirtNumber}</Text>
+        <div className="w-full flex h-full flex-col gap-1 px-2 py-0 items-center justify-between">
+          <Title type="h4">{namePlayer}</Title>
+          <Image src={imagePlayer.src} alt={imagePlayer.alt} className="" />
+          <Text size="l">
+            posicion <Span size="s">{positionPlayer}</Span>
+          </Text>
+          <Text size="l">
+            Dorsal
+            <Span size="s"> {shirtNumber}</Span>
+          </Text>
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 mt-auto">
           {rightStats.map(({ label, value }) => (
-            <StatBox title={label} value={value} textPosition="right" />
+            <StatBox title={label} value={value} color="blue" textPosition="right" />
           ))}
         </div>
       </div>
