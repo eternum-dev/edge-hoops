48fcad2b7c2267b0130f4f302e4155269dd886ab
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-01 22:35:40 -0400

    refactor(graph-card): add `className`prop and change styles

--- src/component/organisms/GraphCard.tsx
+++ src/component/organisms/GraphCard.tsx
@@ -3,61 +3,72 @@ import type { MatchGraphData, TeamGraphData } from "../../types";
 import { Button, DashedLine, ScuareColor, Title } from "../atoms";
 import { GraphQuarter, MatchUpTeam } from "../molecules";
 
-type GraphCardProps = MatchGraphData | TeamGraphData;
+export type GraphCardProps = MatchGraphData | TeamGraphData;
 
 export const GraphCard: React.FC<GraphCardProps> = (props) => {
-  const { buttons, graphs, typeGraph, title } = props;
+  const { buttons, graphs, typeGraph, title, className = "" } = props;
 
   const isTitle =
     title ??
     (typeGraph === "match" ? "grafico del partido" : "grafico de equipo");
 
   return (
-    <section className="bg-neutral-100 rounded-3xl p-5 shadow-xl shadow-neutral-1000">
+    <section
+      className={`w-full flex flex-col gap-4 bg-neutral-100 rounded-3xl p-5 shadow-neutral-700 shadow-md ${className}`}
+    >
       <header>
         <Title>{isTitle}</Title>
       </header>
-      <div>
-        <div className="flex h-[150px] justify-around">
-          {typeGraph === "match"
-            ? graphs.map(({ titleGraph, value }, index) => (
-                <React.Fragment key={titleGraph}>
-                  <div></div>
-                  <GraphQuarter
-                    titleQuarter={titleGraph}
-                    valueGraph={value}
-                    typeGraph={typeGraph}
-                  />
-                  {index !== 3 ? <DashedLine /> : <div></div>}
-                </React.Fragment>
-              ))
-            : graphs.map(({ titleGraph, value }, index) => (
-                <React.Fragment key={titleGraph}>
-                  <div></div>
-                  <GraphQuarter
-                    titleQuarter={titleGraph}
-                    valueGraph={value}
-                    typeGraph={typeGraph}
-                  />
-                  {index !== 3 ? <DashedLine /> : <div></div>}
-                </React.Fragment>
-              ))}
+      <div className="h-full flex flex-col justify-center gap-2.5">
+        <div className="flex h-full flex-col gap-2">
+          <div className="flex h-full justify-around">
+            {typeGraph === "match"
+              ? graphs.map(({ titleGraph, value }, index) => (
+                  <React.Fragment key={titleGraph}>
+                    <div></div>
+                    <GraphQuarter
+                      titleQuarter={titleGraph}
+                      valueGraph={value}
+                      typeGraph={typeGraph}
+                    />
+                    {index !== 3 ? <DashedLine /> : <div></div>}
+                  </React.Fragment>
+                ))
+              : graphs.map(({ titleGraph, value }, index) => (
+                  <React.Fragment key={titleGraph}>
+                    <div></div>
+                    <GraphQuarter
+                      titleQuarter={titleGraph}
+                      valueGraph={value}
+                      typeGraph={typeGraph}
+                    />
+                    {index !== 3 ? <DashedLine /> : <div></div>}
+                  </React.Fragment>
+                ))}
+          </div>
+          <div className="flex justify-between">
+            {buttons.map(({ label }) => (
+              <Button
+                color="secondary"
+                onClick={() => console.log("object")}
+                type="button"
+              >
+                {label}
+              </Button>
+            ))}
+          </div>
         </div>
-        <div className="flex justify-between">
-          {buttons.map(({ label }) => (
-            <Button color="secondary" onClick={() => console.log("object")} type="button">
-              {label}
-            </Button>
-          ))}
-        </div>
+        {typeGraph === "match" && (
+          <footer className="flex justify-center items-center gap-2">
+            <ScuareColor colorBox="primary" />
+            <MatchUpTeam
+              leftTeam={props.leftTeam}
+              rightTeam={props.rightTeam}
+            />
+            <ScuareColor colorBox="secondary" />
+          </footer>
+        )}
       </div>
-      {typeGraph === "match" && (
-        <footer className="flex justify-center items-center gap-2 mt-2.5">
-          <ScuareColor colorBox="primary" />
-          <MatchUpTeam leftTeam={props.leftTeam} rightTeam={props.rightTeam} />
-          <ScuareColor colorBox="secondary" />
-        </footer>
-      )}
     </section>
   );
 };
