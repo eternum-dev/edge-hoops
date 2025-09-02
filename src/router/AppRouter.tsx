5975d91b5a7537ae63c67bc428faa14768fbde3e
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-01 22:48:29 -0400

    feat(router): add `search-game` and `search-team` routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -5,15 +5,28 @@ import {
   FavoritesPage,
   GameDetailPage,
   TeamPage,
+  SearchGamePage,
+  SearchTeamPage,
 } from "../component/pages";
 
 export const AppRouter = () => {
   return (
     <Routes>
+      {/* home */}
       <Route path={"/"} element={<HomeDashboardPage />} />
+
+      {/* games */}
+      <Route path={"/search-game"} element={<SearchGamePage />} />
       <Route path={"/game"} element={<GameDetailPage />} />
+
+      {/* teams */}
+      <Route path={"/search-team"} element={<SearchTeamPage />} />
       <Route path={"/team"} element={<TeamPage />} />
+
+      {/* errors */}
       <Route path={"/404"} element={<Error404Page />} />
+      
+      {/* other */}
       <Route path={"/favorites"} element={<FavoritesPage />} />
     </Routes>
   );
