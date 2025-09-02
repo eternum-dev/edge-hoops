425c5f86aeedaa53a8486859546f6d74c3dd13ba
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-01 22:31:31 -0400

    feat(drop-down-menu): add `placeholder`prop and export interface

--- src/component/organisms/DropDownMenu.tsx
+++ src/component/organisms/DropDownMenu.tsx
@@ -2,17 +2,19 @@ import type { DropDownOptions } from "../../types";
 import { Option } from "../atoms";
 import { DropDownSelect } from "../molecules";
 
-interface DropDownMenuProps {
+export interface DropDownMenuProps {
   optionData: DropDownOptions[];
   className?: string;
+  placeholder?: string
 }
 
 export const DropDownMenu: React.FC<DropDownMenuProps> = ({
   optionData,
   className,
+  placeholder = "Selecciona tu equipo"
 }) => {
   return (
-    <DropDownSelect label="Selecciona tú equipo" className={className}>
+    <DropDownSelect label={placeholder} className={className} >
       <div className="flex flex-col gap-0 px-1.5 py-2.5 bg-neutral-100 z-10 relative bottom-0 rounded-b-xs max-h-60 overflow-y-scroll scroll-smooth">
         {optionData.map(({ label, team }, index) => (
           <Option textPosition="left" key={index} icon={team}>
