123146034dfca8e3a4e0ea912b3de2ff060f4f91
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-02 23:48:27 -0400

    refactor(select-game): add `title`, `className` props  and implement text component with `title` prop

--- src/component/organisms/SelectGameMatch.tsx
+++ src/component/organisms/SelectGameMatch.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../atoms";
+import { Button, Title } from "../atoms";
 import { DropDownMenu, type DropDownMenuProps } from "./DropDownMenu";
 
 export interface SelectGameMatchProps {
@@ -6,6 +6,8 @@ export interface SelectGameMatchProps {
   visitorTeam: DropDownMenuProps;
   season: DropDownMenuProps;
   handleSearchGame: () => void;
+  title?: string;
+  className?: string;
 }
 
 export const SelectGameMatch: React.FC<SelectGameMatchProps> = ({
@@ -13,15 +15,22 @@ export const SelectGameMatch: React.FC<SelectGameMatchProps> = ({
   visitorTeam,
   season,
   handleSearchGame,
+  title = "Selecciona los equipos",
+  className = "",
 }) => {
   return (
-    <div className="flex gap-4 p-5 rounded-3xl shadow-md shadow-neutral-700 bg-neutral-100 col-span-12 self-center h-80">
-      <DropDownMenu {...homeTeam} />
-      <DropDownMenu {...visitorTeam} />
-      <DropDownMenu {...season} />
-      <Button onClick={handleSearchGame} color="secondary">
-        Buscar
-      </Button>
+    <div
+      className={`flex flex-col  gap-4 p-5 rounded-3xl shadow-md shadow-neutral-700 bg-neutral-100  self-center max-h-[300px] ${className}`}
+    >
+      <Title>{title}</Title>
+      <div className="flex gap-3 h-80">
+        <DropDownMenu {...homeTeam} />
+        <DropDownMenu {...visitorTeam} />
+        <DropDownMenu {...season} />
+        <Button onClick={handleSearchGame} color="secondary" size="max">
+          Buscar
+        </Button>
+      </div>
     </div>
   );
 };
