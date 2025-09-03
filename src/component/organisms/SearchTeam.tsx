73a0f703042de2f793e6825a1720abcabefdef6c
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-02 23:42:00 -0400

    refactor(search-team): add `className` prop

--- src/component/organisms/SearchTeam.tsx
+++ src/component/organisms/SearchTeam.tsx
@@ -1,30 +1,33 @@
 import type { ShortTeamNames } from "../../types";
-import { Button, InputSearch } from "../atoms";
+import { Button, InputSearch, Title } from "../atoms";
 import { DropDownMenu } from "./DropDownMenu";
 
-interface SearchTeamProps {
+export interface SearchTeamProps {
   placeholder?: string;
   optionsData: { label: string; icon?: ShortTeamNames }[];
   onClick: () => void;
+  className?: string;
 }
 
 export const SearchTeam: React.FC<SearchTeamProps> = ({
   placeholder = "Ingrese un equipo",
   optionsData,
   onClick,
+  className = "",
 }) => {
   return (
     <form
-      className="grid grid-cols-5 gap-4 bg-neutral-100 shadow-md shadow-neutral-700 p-5  rounded-[30px]"
+      className={`grid grid-cols-5 items-start gap-4 bg-neutral-100 shadow-md shadow-neutral-700 p-5  rounded-[30px] ${className}`}
       onSubmit={(sub) => {
         sub.preventDefault();
-        
+
         const input = sub.currentTarget.elements.namedItem(
           "inputSearch"
         ) as HTMLInputElement;
         console.log(input.value);
       }}
     >
+      <Title className="col-span-5"> Busca tu equipo </Title>
       <InputSearch
         placeholder={placeholder}
         className="col-span-2"
