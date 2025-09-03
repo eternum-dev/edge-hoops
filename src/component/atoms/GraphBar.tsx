c426f9ed46004181dd5ca5ec6e5852eda0c59f92
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-02 23:28:18 -0400

    refactor(graph-bar): change max width button from 70px to 50px

--- src/component/atoms/GraphBar.tsx
+++ src/component/atoms/GraphBar.tsx
@@ -10,9 +10,9 @@ interface GraphBarProps {
 
 const BaseStyled: Record<TypeColor, string> = {
   primary:
-    "w-full min-w-[15px] max-w-[70px] h-full rounded-2xs bg-primary-300 drop-shadow-neutral-1000 drop-shadow-(--shadow-1) ",
+    "w-full min-w-[15px] max-w-[50px] h-full rounded-2xs bg-primary-300 drop-shadow-neutral-1000 drop-shadow-(--shadow-1) ",
   secondary:
-    "w-full min-w-[15px] max-w-[70px] h-full rounded-2xs bg-secondary-300 drop-shadow-neutral-900 drop-shadow-sm",
+    "w-full min-w-[15px] max-w-[50px] h-full rounded-2xs bg-secondary-300 drop-shadow-neutral-900 drop-shadow-sm",
 };
 
 export const GraphBar: React.FC<GraphBarProps> = ({
