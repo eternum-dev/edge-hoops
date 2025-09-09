ecb5317bd3428de22e950c0930f51632750df9f9
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-09 20:26:47 -0300

    feat(team-history): add `className` prop

--- src/component/organisms/TeamHistoryTable.tsx
+++ src/component/organisms/TeamHistoryTable.tsx
@@ -3,19 +3,23 @@ import { ColumTable } from "../molecules";
 import type { DefinitionProps, TableData } from "../../types";
 import { StatsGlossary } from "./StatsGlossary";
 
-interface TeamHistoryTableProps {
+export interface TeamHistoryTableProps {
   title?: string;
   tableData: TableData[];
   glossary: DefinitionProps[];
+  className?: string;
 }
 
 export const TeamHistoryTable: React.FC<TeamHistoryTableProps> = ({
   title = "Ultimos 5 juegos",
   tableData = [],
   glossary = [],
+  className = "",
 }) => {
   return (
-    <section className="flex flex-col w-full gap-4 bg-neutral-100 p-5 rounded-3xl shadow-md shadow-neutral-700">
+    <section
+      className={`flex flex-col w-full gap-4 bg-neutral-100 p-5 rounded-3xl shadow-md shadow-neutral-700 ${className}`}
+    >
       <header className="flex justify-between items-center">
         <Title type="h2">{title}</Title>
       </header>
