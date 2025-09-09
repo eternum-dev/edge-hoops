a1a88429764de77c73d1577784a99271425acb23
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-08 22:16:18 -0300

    refactor(definition): change direccion container and add margin top auto in `fullWord` text component

--- src/component/molecules/Definition.tsx
+++ src/component/molecules/Definition.tsx
@@ -6,11 +6,11 @@ export const Definition: React.FC<DefinitionProps> = ({
   fullWord,
 }) => {
   return (
-    <div className="bg-neutral-100 flex flex-col">
-      <Text className="!text-neutral-700" size="m">
+    <div className="bg-neutral-100 flex ">
+      <Text className="!text-neutral-700" size="l">
         {abbreviation}
       </Text>
-      <Text className="relative left-1" size="m">{fullWord}</Text>
+      <Text className="relative left-1 mt-auto" size="m">{fullWord}</Text>
     </div>
   );
 };
