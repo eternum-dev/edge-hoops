9cd0237aabd822d48de04e85b61e3bc4357f8a06
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-09 20:36:27 -0300

    feat(templates): add GameDetailTemplate component with match, homeMvp,  awayMvp, graph, headTohead and player props

---
+++ src/component/templates/GameDetailTemplate.tsx
@@ -0,0 +1,41 @@
+import {
+  GameCard,
+  GraphCard,
+  HeadToHeadTable,
+  MvpCard,
+  PlayersStadisticTable,
+  type GameCardProps,
+  type GraphCardProps,
+  type HeadToHeadTableProps,
+  type MvpCardProps,
+  type PlayersStadisticTableProps,
+} from "../organisms";
+
+export interface GameDetailTemplateProps {
+  match: GameCardProps;
+  homeMvp: MvpCardProps;
+  awayMvp: MvpCardProps;
+  graph: GraphCardProps;
+  headTohead: HeadToHeadTableProps;
+  player: PlayersStadisticTableProps;
+}
+
+export const GameDetailTemplate: React.FC<GameDetailTemplateProps> = ({
+  match,
+  homeMvp,
+  awayMvp,
+  graph,
+  headTohead,
+  player,
+}) => {
+  return (
+    <div className="grid grid-cols-12  h-full max-h-lvh gap-4 p-5">
+      <GameCard className="col-span-12" {...match} />
+      <MvpCard {...homeMvp} className="col-span-4" />
+      <GraphCard {...graph} className="col-span-4" />
+      <MvpCard {...awayMvp} className="col-span-4" />
+      <HeadToHeadTable {...headTohead} className="col-span-6 " />
+      <PlayersStadisticTable {...player} className="col-span-6 " />
+    </div>
+  );
+};
