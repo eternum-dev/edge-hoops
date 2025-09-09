ee2caf12138eaa682893a2f205372679ebfa7ecb
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-08 22:35:00 -0300

    refactor(select-game): replace flex box  for grid for better distribution

--- src/component/organisms/SelectGameMatch.tsx
+++ src/component/organisms/SelectGameMatch.tsx
@@ -20,17 +20,17 @@ export const SelectGameMatch: React.FC<SelectGameMatchProps> = ({
 }) => {
   return (
     <div
-      className={`flex flex-col  gap-4 p-5 rounded-3xl shadow-md shadow-neutral-700 bg-neutral-100  self-center max-h-[300px] ${className}`}
+      className={`flex flex-col gap-4 p-5 rounded-3xl shadow-md shadow-neutral-700 bg-neutral-100  self-center max-h-[300px] ${className}`}
     >
       <Title>{title}</Title>
-      <div className="flex gap-3 h-80">
-        <DropDownMenu {...homeTeam} />
-        <DropDownMenu {...visitorTeam} />
-        <DropDownMenu {...season} />
-        <Button onClick={handleSearchGame} color="secondary" size="max">
+      <form className="grid grid-cols-12 gap-3 h-80">
+        <DropDownMenu {...homeTeam} className="col-span-6" />
+        <DropDownMenu {...visitorTeam} className="col-span-6" />
+        <DropDownMenu {...season} className="col-span-12 max-w-[320px]" />
+        <Button onClick={handleSearchGame} color="secondary" size="max" className="col-span-2">
           Buscar
         </Button>
-      </div>
+      </form>
     </div>
   );
 };
