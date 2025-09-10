8880ec46480e5537037b0507b1a4ded6f3073f68
eternum-dev <alejandro.thon.j@gmail.com>
2025-09-09 21:11:09 -0300

    refactor: change form label to div

--- src/component/organisms/SelectGameMatch.tsx
+++ src/component/organisms/SelectGameMatch.tsx
@@ -23,14 +23,14 @@ export const SelectGameMatch: React.FC<SelectGameMatchProps> = ({
       className={`flex flex-col gap-4 p-5 rounded-3xl shadow-md shadow-neutral-700 bg-neutral-100  self-center max-h-[300px] ${className}`}
     >
       <Title>{title}</Title>
-      <form className="grid grid-cols-12 gap-3 h-80">
+      <div className="grid grid-cols-12 gap-3 h-80">
         <DropDownMenu {...homeTeam} className="col-span-6" />
         <DropDownMenu {...visitorTeam} className="col-span-6" />
         <DropDownMenu {...season} className="col-span-12 max-w-[320px]" />
         <Button onClick={handleSearchGame} color="secondary" size="max" className="col-span-2">
           Buscar
         </Button>
-      </form>
+      </div>
     </div>
   );
 };
